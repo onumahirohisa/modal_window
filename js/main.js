e90d74d404ae5e2ecf6722597a9c0f03eeacc4c9
kyoro <hirohisaonuma@icloud.com>
2020-09-22 21:01:21 +0900

    gaming

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+{
+  const open = document.getElementById("open");
+  const mask = document.getElementById("mask");
+  const modal = document.getElementById("modal");
+  const close = document.getElementById("close");
+
+  open.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+  });
+
+  close.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+  });
+
+  mask.addEventListener("click", () => {
+    // modal.classList.add("hidden");
+    // mask.classList.add("hidden");
+    close.click();
+  });
+}
